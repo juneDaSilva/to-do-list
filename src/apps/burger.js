f4f5deb9f25952936793b07b7433c3697b1d75c4
June <adasilva.html@gmail.com>
2023-01-06 18:21:08 -0500

    Make sidebar dropdown functional

--- src/apps/burger.js
+++ src/apps/burger.js
@@ -1,5 +1,6 @@
 const burger = document.querySelector(".burger-menu");
 const sidebar = document.querySelector(".sidebar");
+const main = document.querySelector(".main");
 
 const toggleBurger = () => {
   burger.classList.toggle("burger-active");
@@ -9,10 +10,15 @@ const toggleSidebar = () => {
   sidebar.classList.toggle("side-show");
 };
 
+const toggleMain = () => {
+  main.classList.toggle("inactive-main");
+};
+
 const burgerListen = () => {
   burger.addEventListener("click", () => {
     toggleBurger();
     toggleSidebar();
+    toggleMain();
   });
 };
 
